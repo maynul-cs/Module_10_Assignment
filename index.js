e8d2f9208fed15a6305905df981a497d16cfe6b2
Maynul Islam <maynulislammishu@gmail.com>
2024-12-09 21:39:10 +0600

    1st Problem Solved

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+// 1) Write a function named calculateDifference that takes two arguments and returns the difference between the first and second arguments.
 
+function calculateDifference(x,y){
+    return x-y;
+}
+let result=calculateDifference(85,40);
+console.log(result);
