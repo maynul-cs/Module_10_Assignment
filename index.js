a2f2b2fef545af97ce3fcf693640e2d982efced3
Maynul Islam <maynulislammishu@gmail.com>
2024-12-09 21:46:57 +0600

    2nd Problem Solved

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function calculateDifference(x,y){
 }
 let result=calculateDifference(85,40);
 console.log(result);
+
+
+// 2) Write a function named isOdd that takes one argument and returns true if the number is odd, and false if it is not.
+
+function isOdd(x){
+    return x % 2 !== 0;
+}
+console.log(isOdd(20));
